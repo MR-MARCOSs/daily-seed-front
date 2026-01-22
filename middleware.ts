31711b8c7bee4560e3b6ac286b1978ae118f2e5e
MR-MARCOSs <marcosdudu001@gmail.com>
2026-01-22 15:53:09 -0300

    refactor: change in tsconfig.node.json to include middleware

--- middleware.ts
+++ middleware.ts
@@ -5,47 +5,29 @@ export const config = {
   matcher: '/api-backend/:path*',
 };
 
-export default async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  const newPath = pathname.replace('/api-backend', '');
-  const backendUrl = process.env.BACKEND_URL; 
+export default function middleware(request: NextRequest) {
+  
+  const backendUrl = process.env.BACKEND_URL;
 
   if (!backendUrl) {
-    return NextResponse.json({ error: 'Env var missing' }, { status: 500 });
+    
+    return NextResponse.json(
+      { error: 'BACKEND_URL config missing' },
+      { status: 500 }
+    );
   }
-
+  
+  const pathname = request.nextUrl.pathname;
+  const newPath = pathname.replace(/^\/api-backend/, '');
   const targetUrl = new URL(newPath + request.nextUrl.search, backendUrl);
 
-  try {
-    console.log(`Tentando conectar em: ${targetUrl.toString()}`);
-    
-    
-    const response = await fetch(targetUrl.toString(), {
-      method: request.method,
-      headers: {
-        'Content-Type': 'application/json',
-        
-        
-      },
-      
-    });
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set('Host', new URL(backendUrl).host);
+  requestHeaders.set('X-Forwarded-Proto', 'https');
 
-    console.log(`Conexão bem sucedida! Status: ${response.status}`);
-    
-    
-    
-    const data = await response.json();
-    return NextResponse.json(data, { status: response.status });
-
-  } catch (error: any) {
-    
-    console.error('Erro de Conexão:', error);
-    
-    return NextResponse.json({
-      erro_diagnostico: 'A Vercel não conseguiu conectar no seu VPS',
-      motivo_tecnico: error.message,
-      causa: error.cause,
-      tentou_conectar_em: targetUrl.toString()
-    }, { status: 502 }); 
-  }
+  return NextResponse.rewrite(targetUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
 }
