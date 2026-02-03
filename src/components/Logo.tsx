502a668197a98371c0dbee91ad13ce2fc7168b77
MR-MARCOSs <marcosdudu001@gmail.com>
2026-02-03 17:52:58 -0300

    refactor: change icon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div className="text-center mt-5 p-4 bg-white rounded-xl shadow-sm">
       <img 
-        src="/logo.png" 
+        src="/logo.png"
         alt="Logo Conecta" 
         className="max-w-48 h-auto cursor-pointer transition-transform duration-300 hover:scale-105 mx-auto"
       />
